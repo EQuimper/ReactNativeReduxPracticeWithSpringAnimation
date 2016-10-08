5503287155482e9c6d591a371b6ebbddb3b39cfc
EQuimper <quimperemanuel@gmail.com>
2016-10-08 14:35:23 -0400

    Make a single list item

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { CardSection } from './common';
 
 class ListItem extends Component {
   render() {
     const { title } = this.props.library;
+    const { titleStyle } = styles;
     return (
-      <View>
-        <Text>{title}</Text>
-      </View>
+      <CardSection>
+        <Text style={titleStyle}>{title}</Text>
+      </CardSection>
     );
   }
 }
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
 
 export default ListItem;
